feb476144393615c2ac22ed84d472a41fc4afffc
Lukas Karlsson <karlsson@altissimo.io>
2023-04-23 14:37:13 -0400

    Added secret.

--- main.py
+++ main.py
@@ -1,33 +1,29 @@
-# Copyright 2020 Google, LLC.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START cloudrun_helloworld_service]
-# [START run_helloworld_service]
+"""Badge Reader Cloud Run Service"""
 import os
 
 from flask import Flask
+from google.cloud import secretmanager_v1
 
 app = Flask(__name__)
 
 
+def get_secret(secret, version="latest"):
+    """Get the secret value from the environment."""
+    client = secretmanager_v1.SecretManagerServiceClient()
+    project = "gdg-cloud-boston"
+    secret_version_name = f'projects/{project}/secrets/{secret}/versions/{version}'
+    request = {"name": secret_version_name}
+    return client.access_secret_version(request=request).payload.data.decode("UTF-8")
+
+
 @app.route("/")
-def hello_world():
+def index():
+    """Display the main index page."""
     name = os.environ.get("NAME", "World")
+    secret = get_secret("badgetracker-oauth-client-secret")
+    print(secret)
     return "Hello {}!".format(name)
 
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
-# [END run_helloworld_service]
-# [END cloudrun_helloworld_service]

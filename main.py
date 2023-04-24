ea14bf4d0f322f5d9943408c468f3c844d5c15b9
Lukas Karlsson <karlsson@altissimo.io>
2023-04-23 23:22:05 -0400

    Added full login process.

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from flask_login import login_required
 from flask_login import login_user
 from flask_login import LoginManager
 from flask_login import logout_user
+from flask_login import UserMixin
+
+from google.cloud import firestore
 from google.cloud import secretmanager_v1
 from oauthlib.oauth2 import WebApplicationClient
 
@@ -20,6 +23,16 @@ app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
 DISCOVERY_URL = "https://accounts.google.com/.well-known/openid-configuration"
+
+
+class User(UserMixin):
+    """User class."""
+    def __init__(self, user):
+        """Initialize."""
+        self.id = user.get("sub")
+        self.name = user.get("name")
+        self.email = user.get("email")
+        self.profile_pic = user.get("picture")
 
 
 def get_google_provider_cfg():
@@ -34,6 +47,29 @@ def get_secret(secret, version="latest"):
     secret_version_name = f'projects/{project}/secrets/{secret}/versions/{version}'
     req = {"name": secret_version_name}
     return client.access_secret_version(request=req).payload.data.decode("UTF-8")
+
+
+def get_user(user_id):
+    """Returna user from Firestore."""
+    client = firestore.Client()
+    user = client.collection("users").document(user_id).get()
+    if user.exists:
+        return User(user.to_dict())
+    return None
+
+
+def save_user(user):
+    """Save a user to firestore."""
+    email = user.get("email")
+    name = user.get("name")
+    # picture = user.get("picture")
+    sub = user.get("sub")
+    print(f"{name} <{email}> [{sub}]")
+
+    client = firestore.Client()
+    client.collection("users").document(sub).set(user)
+
+    return
 
 
 OAUTH_CLIENT_INFO = json.loads(get_secret("badgetracker-oauth-client-secret"))
@@ -55,7 +91,7 @@ client = WebApplicationClient(CLIENT_ID)
 def load_user(user_id):
     """Load a user."""
     print(f"Loading user: {user_id}")
-    return user_id
+    return get_user(user_id)
 
 
 @app.route("/")
@@ -74,25 +110,6 @@ def index():
         return '<a class="button" href="/login">Google Login</a>'
 
 
-@app.route("/login")
-def login():
-    """Login with Google."""
-    # Find out what URL to hit for Google login
-    google_provider_cfg = get_google_provider_cfg()
-    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
-
-    redirect_uri = request.url_root + "callback"
-
-    # Use library to construct the request for Google login and provide
-    # scopes that let you retrieve user's profile from Google
-    request_uri = client.prepare_request_uri(
-        authorization_endpoint,
-        redirect_uri=redirect_uri,
-        scope=["openid", "email", "profile"],
-    )
-    return redirect(request_uri)
-
-
 @app.route("/callback")
 def callback():
     """Handle the callback."""
@@ -107,8 +124,8 @@ def callback():
     # Prepare and send a request to get tokens! Yay tokens!
     token_url, headers, body = client.prepare_token_request(
         token_endpoint,
-        authorization_response=request.url,
-        redirect_url=request.base_url,
+        authorization_response=request.url.replace("http://", "https://"),
+        redirect_url=request.base_url.replace("http://", "https://"),
         code=code
     )
     token_response = requests.post(
@@ -126,10 +143,43 @@ def callback():
     # including their Google profile image and email
     userinfo_endpoint = google_provider_cfg["userinfo_endpoint"]
     uri, headers, body = client.add_token(userinfo_endpoint)
-    userinfo_response = requests.get(uri, headers=headers, data=body)
+    user_info = requests.get(uri, headers=headers, data=body).json()
 
-    print(userinfo_response.json())
-    return "ok"
+    save_user(user_info)
+
+    user = User(user_info)
+
+    # Begin user session by logging the user in
+    login_user(user)
+
+    # Send user back to homepage
+    return redirect(url_for("index"))
+
+
+@app.route("/login")
+def login():
+    """Login with Google."""
+    # Find out what URL to hit for Google login
+    google_provider_cfg = get_google_provider_cfg()
+    authorization_endpoint = google_provider_cfg["authorization_endpoint"]
+
+    # Use library to construct the request for Google login and provide
+    # scopes that let you retrieve user's profile from Google
+    request_uri = client.prepare_request_uri(
+        authorization_endpoint,
+        redirect_uri=f"{request.url_root}callback".replace("http://", "https://"),
+        scope=["openid", "email", "profile"],
+    )
+    # print(request_uri)
+    return redirect(request_uri)
+
+
+@app.route("/logout")
+@login_required
+def logout():
+    """Logout the user."""
+    logout_user()
+    return redirect(url_for("index"))
 
 
 if __name__ == "__main__":
